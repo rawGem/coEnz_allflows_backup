bd0c9edaf7d5c30f836736e8c9135e0195f94d3d
Grapefruit <grapefruit@Grapefruits-MacBook-Pro.local>
2015-12-25 18:46:02 -0500

    Draws all periods for order 11

--- client/draw_parallel.js
+++ client/draw_parallel.js
@@ -10,7 +10,7 @@ instance.autorun(
 function(){
    
   if (FlowRouter.getParam("order_number")) {
-    var order = FlowRouter.getParam("order_number")
+  var order = FlowRouter.getParam("order_number")
   }
 
   if (order === "11") {
@@ -19,78 +19,147 @@ function(){
 
   if (instance.subscriptionsReady()) {
 
-  var Routes, viewingHeight, viewingWidth, linkScale, linkGutter,
+  var Routes, height, width, boxSide, linkScale, linkGutter,
       currentYpos, lowVolume;
-    
-    Object.keys(data).forEach(
-    function(el) {
-      if (el === "p1") {
-        var d = data[el].ord11
-        Routes = ParseUtils.processRoutes(d)
-      }
-    })
+
+  Routes = [] 
   
+  Object.keys(data).forEach(
+  function(el) {
+    if (el !== "_id") {
+      var d = data[el].ord11
+      Routes.push(ParseUtils.processRoutes(d))
+    }
+  })
+
+  console.log(Routes)
+  //Routes = Routes[0]
+  
+  height        = 1240;
+  width         = 930;
+  boxSide       = 300;
   routeKeys     = Object.keys(Routes);
   currentYpos   = 0;
   linkScale     = d3.scale.linear().domain([0,70000]).range([1,50]);
-  viewingHeight = 500;
-  viewingWidth  = 900;
   lowVolume     = 1;
   linkGutter    = 10;
 
   var svg = d3.select("body").append("svg")
-        .attr("width", viewingWidth)
-        .attr("height", viewingHeight);
+        .attr("width", width)
+        .attr("height", height);
 
   
-  var rects = svg.selectAll("g").data(routeKeys).enter().append("g");
+  var groups = svg.selectAll("g").data(Routes).enter().append("g")
+        .attr("transform", function(d,i) {
+        return "translate("+(boxSide*(i%3)+10)+","
+                           + boxSide*Math.floor(i/3)
+                           + ")"
+        })
+        
 
-  // draw paths
-  rects.append("rect") 
-        .attr("x", function(d,i) {return 10})
+  
+
+  // draw links
+  groups.each(
+  function(data,index){ 
+  d3.select(this).selectAll("rect")
+        .data(function(d,i) {
+        currentYpos = 0;
+        return Object.keys(d)
+        })
+       .enter()
+        .append("rect")
+        .attr("fill", "#bbb")
+        .attr("x", function(d,i) {
+        console.log(data[d]) 
+        return 10})
         .attr("y", function(d,i) {
           if (currentYpos === 0) {
-            Routes[d]["dy"] = 0
-            currentYpos += linkScale(Routes[d].segments[0]["Product volume"])
+            data[d]["dy"] = 0
+            currentYpos += linkScale(data[d].segments[0]["Product volume"])
             return 0
           }
           else {
-            currentYpos += linkScale(Routes[d].segments[0]["Product volume"])+linkGutter
-            Routes[d]["dy"] = currentYpos - linkScale(Routes[d].segments[0]["Product volume"])
-            return currentYpos - linkScale(Routes[d].segments[0]["Product volume"])
+            currentYpos += linkScale(data[d].segments[0]["Product volume"])+linkGutter
+            data[d]["dy"] = currentYpos - linkScale(data[d].segments[0]["Product volume"])
+            return currentYpos - linkScale(data[d].segments[0]["Product volume"])
           }
         })
         .attr("height", function(d,i) {
-            return linkScale(Routes[d].segments[0]["Product volume"])
+            console.log( "height", linkScale(data[d].segments[0]["Product volume"]) )
+            return linkScale(data[d].segments[0]["Product volume"])
         })
         .attr("width", 300)
         .attr("fill", function(d,i) {
-        var seg = Routes[d].segments[0]
+        var seg = data[d].segments[0]
         return seg["Product volume"] < lowVolume ? "red" : "#eeeeef"
         }).on("mouseenter", function(d){
-           svg.append("rect")
-              .attr("x", 310)
-              .attr("y", Routes[d].dy)
-              .attr("width", 20)
-              .attr("height", this.getAttribute("height"))
-              .attr("fill", "orange")
+           console.log(data[d])
+           //svg.append("rect")
+           //   .attr("x", 310)
+           //   .attr("y", data[d].dy)
+           //   .attr("width", 20)
+           //   .attr("height", this.getAttribute("height"))
+           //   .attr("fill", "orange")
            })
+        
+          
+  
+  })
+  //groups.selectAll("rect").data(
+  //                         function(d,i) {
+  //                         currentPos = 0;
+  //                         console.log("d", d)
+  //                         return Object.keys(d)})
+  //    .enter()
+  //      .append("rect")
+  //      .attr("fill", function(d,i){console.log(d,this)})
+        //.append("rect")
+        //.attr("x", function(d,i) {return 10})
+        //.attr("y", function(d,i) {
+        //  if (currentYpos === 0) {
+        //    Routes[i][d]["dy"] = 0
+        //    currentYpos += linkScale(Routes[i][d].segments[0]["Product volume"])
+        //    return 0
+        //  }
+        //  else {
+        //    currentYpos += linkScale(Routes[i][d].segments[0]["Product volume"])+linkGutter
+        //    Routes[i][d]["dy"] = currentYpos - linkScale(Routes[i][d].segments[0]["Product volume"])
+        //    return currentYpos - linkScale(Routes[i][d].segments[0]["Product volume"])
+        //  }
+        //})
+        //.attr("height", function(d,i) {
+        //    console.log( "height", linkScale(Routes[i][d].segments[0]["Product volume"]) )
+        //    return linkScale(Routes[i][d].segments[0]["Product volume"])
+        //})
+        //.attr("width", 300)
+        //.attr("fill", function(d,i) {
+        //var seg = Routes[i][d].segments[0]
+        //return seg["Product volume"] < lowVolume ? "red" : "#eeeeef"
+        //}).on("mouseenter", function(d){
+        //   svg.append("rect")
+        //      .attr("x", 310)
+        //      .attr("y", Routes[i][d].dy)
+        //      .attr("width", 20)
+        //      .attr("height", this.getAttribute("height"))
+        //      .attr("fill", "orange")
+        //   })
 
   // label paths
-  rects.append("text")
-        .attr("x", 125)
-        .attr("dy", ".35em")
-        .attr("text-anchor", "start")
-        .attr("y",    function(d,i) {
-        return Routes[d].dy + linkScale(Routes[d].segments[0]["Product volume"])/2
-        })
-        .text(function(d,i) {
-        return Routes[d].path.join(" ")  
-        })
+  //groups.append("text")
+  //      .attr("x", 125)
+  //      .attr("dy", ".35em")
+  //      .attr("text-anchor", "start")
+  //      .attr("y",    function(d,i) {
+  //      return Routes[i][d].dy + linkScale(Routes[i][d].segments[0]["Product volume"])/2
+  //      })
+  //      .text(function(d,i) {
+  //      return Routes[i][d].path.join(" ")  
+  //      })
 
-  svg.on("mousemove", function() {
-    rects.attr("opacity", function(d) {console.log(Routes[d])}) 
-  })
+  //svg.on("mousemove", function() {
+  //  groups.attr("opacity", function(d,i) {console.log(Routes[i][d])}) 
+  //})
 
   } // subs ready 
 
